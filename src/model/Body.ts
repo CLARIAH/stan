28f19fe82b3b6a1760ca4efaf44a5347700f7e75
Jaap Blom <jblom@beeldengeluid.nl>
2019-10-03 15:54:06 +0200

    added purpose enum

--- src/model/Body.ts
+++ src/model/Body.ts
@@ -1,6 +1,14 @@
+export enum Purpose {
+	CLASSIFYING = 'classifying',
+	LINKING = 'linking',
+	COMMENTING = 'commenting',
+	TAGGING = 'tagging',
+	CORRECTING = 'correcting'
+}
+
 export default class Body {
 
-	constructor(public type: string, public purpose: string, public format: string, public value: any) {
+	constructor(public type: string, public purpose: Purpose, public format: string, public value: any) {
 		this.type = type;
 		this.purpose = purpose;
 		this.format = format;
