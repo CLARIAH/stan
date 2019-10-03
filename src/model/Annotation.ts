a5cea53ca3ec762bf3931f7c7e49a75716f295e7
Jaap Blom <jblom@beeldengeluid.nl>
2019-10-03 15:04:19 +0200

    added TargetRegistry (formarly known as the resource store/index); trying to fill in the main STAN functions

--- src/model/Annotation.ts
+++ src/model/Annotation.ts
@@ -1,9 +1,15 @@
 import Target from './Target';
 import Body from './Body';
 
+//TODO what are the possible motivations?
+export enum Motivation {
+	BOOKMARKING = 'bookmarking'
+}
+
 export default class Annotation {
 
-	constructor(public id: string, public target?: Target, public body?: Body) {
+	constructor(public context: string, public id: string, private type: string, public creator: string, public created: Date,
+		public motivation?: Motivation,	public target?: Target, public body?: Body) {
 		this.id = id;
 		this.target = target;
 		this.body = body;
