a3e892083334e76a4f0a63561ce8bce64512f18c
Marijn Koolen <marijn.koolen@gmail.com>
2019-11-08 17:41:54 +0100

    Add resource model and basic test

---
+++ src/__tests__/test-model-Resource.ts
@@ -0,0 +1,20 @@
+import Resource from "../model/Resource";
+
+const makeRDFaAttributes = () => {
+    return {
+        about: "urn:div=1",
+        resource: "urn:div=1",
+        vocab: "http://boot.huygens.knaw.nl/vgdemo/editionannotationontology.ttl",
+        prefix: null,
+        typeof: "Poem",
+        property: "Work",
+        text: "Some text that doesn't rhyme.",
+    }
+}
+
+test("Resource returns a valid resource", () => {
+    const attrs = makeRDFaAttributes();
+    const node = document.createElement("div");
+    const resource = new Resource(node, attrs.about, attrs.typeof, attrs.property, null, true, attrs.text);
+    expect(resource).not.toBe(null);
+});
---
+++ src/model/Resource.ts
@@ -0,0 +1,17 @@
+
+// RDFa/External resources are indexed to make target generation easy 
+// as well as linking an RDFa target to the DOM.
+
+export default class Resource {
+
+	constructor(public node: Node, public id: string, public rdfType: String | Array<String>, public rdfProperty: any, public rdfaParent: any, public inDOM: Boolean, public text: string) {
+		this.node = node;
+        this.id = id;
+		this.rdfType = rdfType;
+		this.rdfProperty = rdfProperty;
+        this.rdfaParent = rdfaParent;
+        this.inDOM = inDOM;
+        this.text = text;
+	}
+
+}
